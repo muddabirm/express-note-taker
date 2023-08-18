f07c5bd75bf61a186760870ee8d1dd1c0f7a3cbf
muddabirm <33209109+muddabirm@users.noreply.github.com>
2023-08-17 23:46:31 -0500

    main branch

---
+++ Develop/server.js
@@ -0,0 +1,9 @@
+const express = require('express');
+const path = require('path');
+const PORT = process.env.PORT || 3001;
+const app = express();
+
+app.get('/notes',(req,res)=> 
+);
+app.get('api/notes',(req,res)=> )
+app.post('api/notes',(req,res)=> )
